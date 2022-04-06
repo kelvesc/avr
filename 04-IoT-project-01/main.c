b33afb81d084b2cdd53400cadcd6c3a5805c64c6
Kelves <kelves_16@hotmail.com>
2022-04-06 02:01:51 -0300

    NEW: first commit to 14738:IOT project submission.

---
+++ 04-IoT-project-01/main.c
@@ -0,0 +1,14 @@
+#define F_CPU 16000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+        /* SETUP HERE */
+
+        while(1){
+                /* INFINITE LOOP HERE */
+
+        } /* end loop */
+} /* end main */
